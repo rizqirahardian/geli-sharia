cb3e2517e3ff7a0b12e25cc9951ead5de82cbab9
Rizqi Rahardian <rizqi@ekreasi.com>
2020-06-16 09:43:13 +0700

    first commit

---
+++ src/app/services/models/models.ts
@@ -0,0 +1,26 @@
+export class BeliPolis {
+    id: string
+    statusCT: string
+    namaCT: string
+    nomorKTPCT: string
+    dobCT: string
+    genderCT: string
+    namaPP: string
+    dobPP: string
+    genderPP: string
+    nomorTelponPP: string
+    emailPP:string
+    nomorKTPPP: string
+    provinsi: string
+    statusPM: string
+    namaPM: string
+    nomorKTPPM: string
+    dobPM: string
+    genderPM: string
+    namaBank: string
+    nomorBank: string
+    pemilikBank: string
+    product: string
+    premi: string
+    manfaat: string
+}
