52371ce741759998e4c06a918361d9a5f90e2d2d
Rizqi Rahardian <rizqi@ekreasi.com>
2020-06-16 09:46:43 +0700

    updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MenuComponent } from './menu/menu.component'
+import { DashboardComponent } from './pages/dashboard/dashboard.component'
+import { ProductsComponent } from './pages/products/products.component'
+import { DataDiriComponent } from './pages/data-diri/data-diri.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: '404', redirectTo: 'dashboard'},
+  {path: '', component: MenuComponent, children: [
+    {path: 'dashboard', component: DashboardComponent},
+    {path: 'product/:id', component: ProductsComponent},
+    {path: 'data-diri', component: DataDiriComponent},
+  ]},
+  {path: '**', redirectTo: 'dashboard'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsComponent } from './pages/products/products.component';
+import { MenuComponent } from './menu/menu.component';
+import { ModalDirective } from './directives/modal/modal.directive';
+import { DataDiriComponent } from './pages/data-diri/data-diri.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ProductsComponent,
+    MenuComponent,
+    ModalDirective,
+    DataDiriComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [ModalDirective]
 })
 export class AppModule { }
